ffebf8423cf33a7c1f07e38baadc3b74f26dc72f
Juraj Vysvader <juraj.vysvader@gmail.com>
2019-03-24 05:28:38 +0000

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,16 @@
+# For a GUI, better performance, more functions, and easiness to use... Check out: www.unisoftdev.tech
+
+'''
+* @ The Free Version Of a Web Crawler
+* Created By: Juraj Vysvader
+* Author's profile: https://www.linkedin.com/in/jurajvysvader
+                    https://www.linkedin.com/company/unisoftdev 
+* Creation date: 10.3.2019
+* Business website: https://www.unisoftdev.tech
+* @license http://www.gnu.org/copyleft/lgpl.html GNU/GPL
+* @Copyright (C) 2019 Juraj Vysvader. All rights reserved.
+'''
+
 #shared variables
 
 daily_limit = 0
