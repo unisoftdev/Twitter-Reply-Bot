f3ffd6c1f7aaeb5761b225a559fc670b0997e048
Juraj Vysvader <juraj.vysvader@gmail.com>
2019-03-24 05:30:45 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,3 +1,16 @@
+# For a GUI, better performance, more functions, and easiness to use... Check out: www.unisoftdev.tech
+
+'''
+* @ Twitter explorer
+* Created By: Juraj Vysvader
+* Author's profile: https://www.linkedin.com/in/jurajvysvader
+                    https://www.linkedin.com/company/unisoftdev 
+* Creation date: 10.3.2019
+* Business website: https://www.unisoftdev.tech
+* @license http://www.gnu.org/copyleft/lgpl.html GNU/GPL
+* @Copyright (C) 2019 Juraj Vysvader. All rights reserved.
+'''
+
 
 def main():
     import twitter_customer_reply_bot
